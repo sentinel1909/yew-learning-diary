9f5368e4da9b6da31a7bfea308d4cbdae834520c
Jeff Mitchell <sentinel1909@protonmail.com>
2022-03-26 14:16:14 -0700

    Basic page structure and content creation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@
 // pull in the yew crate 
 use yew::prelude::*;
 
+/// struct to represent a diary entry
+struct Entry {
+    id: usize,
+    date: String,
+    title: String,
+    content: String,
+}
+
 /// enum for the Msg type
 enum Msg {
 
@@ -26,11 +34,30 @@ impl Component for App {
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
+        // declare a vector of diary entries
+        let entries = vec![
+            Entry {
+                id: 1,
+                date: String::from("2022-03-26"),
+                title: String::from("It Lives!"),
+                content: String::from("Barebones site up and running.")
+            }
+        ];
         html! {
-            <div>
-                <h1> {"Yew Learning Diary"}</h1>
-                <h2> {"Documenting the journey to learn the Yew framework for WebAssembly apps in Rust"} </h2>
-            </div>
+            <main>
+                <header>
+                    <h1> {"Yew Learning Diary"}</h1>
+                    <h2> {"Documenting the journey to learn the Yew framework for WebAssembly apps in Rust"} </h2>
+                </header>
+                <section>   // render the diary entries, so far there's just one...need to iterate on this eventually
+                    <h3> {&entries[0].date} </h3>
+                    <h3> {&entries[0].title} </h3>
+                    <p> {&entries[0].content} </p>
+                </section>
+                <footer>
+                    <p> { "Copyright 2022 Jeffery D Mitchell All Rights Reserved" } </p>
+                </footer>
+            </main>
         }
     }
 }
