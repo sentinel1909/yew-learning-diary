1174882b116d554bdbc6e9b2ede8e8cdfdcf8c85
Jeff Mitchell <sentinel1909@protonmail.com>
2022-04-12 20:18:39 -0700

    Refactor datastore to make it more "Rusty"

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,14 +1,11 @@
 // src/app.rs
 // to run this app, type "trunk serve --open" at the command line at the top of the yew-learning-diary directory
 
-// pull in the yew crate 
 use yew::prelude::*;
 
-// pull in external modules
 use yewlearningdiary::data::*;
 use yewlearningdiary::components::*;
 
-// root component, gets mounted into index.html
 enum Msg {
 
 }
@@ -27,10 +24,10 @@ impl Component for App {
         false
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {  
-        // the Entry struct has a new method use to pull in entries from the internal data file
-        let entries = Entry::new().iter().map(|entry| html! { 
+    fn view(&self, _ctx: &Context<Self>) -> Html {        
+        let entries = build_entries().into_iter().map(|entry| html! { 
             <article class="article">
+                <p> {&entry.id} </p>
                 <h3> {&entry.date} </h3>
                 <h3> {&entry.title} </h3>
                 <p> {&entry.content} </p>
--- src/lib/data/datastore.rs
+++ src/lib/data/datastore.rs
@@ -1,42 +1,62 @@
 use uuid::Uuid;
 
-// struct to represent a diary entry
-pub struct Entry<'a> {
+pub struct Entry {
     pub id: Uuid,
-    pub date: &'a str,
-    pub title: &'a str,
-    pub content: &'a str,
+    pub date: String,
+    pub title: String,
+    pub content: String,
 }
 
-// impl block which contains a new() method to create individual entries, stored in a vector
-impl Entry<'_> {
-    pub fn new() -> Vec<Entry<'static>> {
-        let data = vec![
-            Entry {
-                id: Uuid::new_v4(),
-                date: "2022-03-26",
-                title: "It Lives!",
-                content: "Barebones site up and running. It's super easy to build a release version with Trunk.  Just type trunk build --release, which creates a /dist folder. Drag all the files in the /dist folder to your web host and you're done."
-            },
-            Entry {
-                id: Uuid::new_v4(),
-                date: "2022-04-03",
-                title: "Displaying Content with Iteration",
-                content: "Diary content is stored in a vector of structs and displayed by iterating over the elements of the vector."
-            },
-            Entry {
-                id: Uuid::new_v4(),
-                date: "2022-04-11",
-                title: "Breaking up the Code",
-                content: "I've refactored the code a bit to take advantage of external modules. I've found this difficult to understand, but have settled on  a structure that I think I get now. Added a link to the GitHub repo for this site. Please feel free to study, critique, bash, use, whatever.",
-            },
-            Entry {
-                id: Uuid::new_v4(),
-                date: "2022-04-12",
-                title: "Functional Components and use_state",
-                content: "Broke the header and footer out into separate components, using Yew's notion of functional components. Added the uuid crate to generate a unique ID for each diary entry. Added the chrono crate and figured out Yew's equivalent of use_state to add the year to the copyright notice in the footer. Added some CSS to start formatting the site."
-            }
-        ];
-        data
+pub struct Entries(Vec<Entry>);
+
+impl Entries {
+    pub fn new() -> Entries {
+        Entries(Vec::new())
+    }
+
+    pub fn add(&mut self, elem: Entry) {
+        self.0.push(elem)
     }
 }
+
+impl IntoIterator for Entries {
+    type Item = Entry;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+pub fn build_entries() -> Entries {
+    let mut built_entries = Entries::new();
+    let entry1 = Entry {
+        id: Uuid::new_v4(),
+        date: "2022-03-26".to_owned(),
+        title: "It lives!".to_owned(),
+        content: "Barebones site up and running. It's super easy to build a release version with Trunk.  Just type trunk build --release, which creates a /dist folder. Drag all the files in the /dist folder to your web host and you're done.".to_owned(),
+    };
+    let entry2 = Entry {
+        id: Uuid::new_v4(),
+        date: "2022-04-03".to_owned(),
+        title: "Breaking up the Code".to_owned(),
+        content: "I've refactored the code a bit to take advantage of external modules. I've found this difficult to understand, but have settled on  a structure that I think I get now. Added a link to the GitHub repo for this site. Please feel free to study, critique, bash, use, whatever.".to_owned(),
+    };
+    let entry3 = Entry {
+        id: Uuid::new_v4(),
+        date: "2022-04-12".to_owned(),
+        title: "Functional Components and use_state".to_owned(),
+        content: "Broke the header and footer out into separate components, using Yew's notion of functional components. Added the uuid crate to generate a unique ID for each diary entry (displayed while I play, but will eventually hide this). Added the chrono crate and figured out Yew's equivalent of use_state to add the year to the copyright notice in the footer. Added some CSS to start formatting the site.".to_owned(),
+    };
+    built_entries.add(entry1);
+    built_entries.add(entry2);
+    built_entries.add(entry3);
+    built_entries
+}
+
+
+
+
+
+
+
